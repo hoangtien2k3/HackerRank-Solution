05158406f82e737c7487cc3fad9006f536653f61
hoangtien2k3 <hoangtien2k3qx1@gmail.com>
2022-08-23 17:57:49 +0700

    C++ HackerRank Solution

---
+++ DataStructre/Tree/6.cpp
@@ -0,0 +1,78 @@
+
+// Binary Search Tree : Insertion
+// https://www.hackerrank.com/challenges/binary-search-tree-insertion/problem?isFullScreen=true
+
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Node {
+    public:
+        int data;
+        Node *left;
+        Node *right;
+        Node(int d) {
+            data = d;
+            left = NULL;
+            right = NULL;
+        }
+};
+
+class Solution {
+    public:
+  	
+  	void preOrder(Node *root) {
+		
+      	if( root == NULL )
+          	return;
+      
+      	std::cout << root->data << " ";
+      	
+      	preOrder(root->left);
+      	preOrder(root->right);
+    }
+
+    Node * insert(Node * root, int data) {
+        if (root == NULL) {
+            Node *newNode = new Node(data);
+            // Node *newNode = (Node*)malloc(sizeof(Node));
+            newNode->left = NULL;
+            newNode->right = NULL;
+            newNode->data = data;
+            return newNode;
+        } else {
+            if (data  < root->data) {
+                root->left = insert(root->left, data);
+            } else if (data > root->data) {
+                root->right = insert(root->right, data);
+            }
+        }
+        return root;
+    }
+    
+    
+};
+
+int main() {
+  
+    Solution myTree;
+    Node* root = NULL;
+    
+    int t;
+    int data;
+
+    std::cin >> t;
+
+    while(t-- > 0) {
+        std::cin >> data;
+        root = myTree.insert(root, data);
+    }
+  	
+    myTree.preOrder(root);
+  
+    return 0;
+}
+
+
+
